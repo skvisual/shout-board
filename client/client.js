2b0529ed88e4f7023da7edd7e4bb28ed3f732313
skvisual <southkvisual@gmail.com>
2020-04-20 19:44:58 -0500

    initial commit

---
+++ client/client.js
@@ -0,0 +1,22 @@
+
+const form= document.querySelector('form');
+const loadingElement = document.querySelector('.loading');
+
+loadingElement.style.display = 'none';
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const content = formData.get('content');
+
+    const tweet = {
+        name,
+        content
+    }
+
+    console.log(tweet)
+    form.style.display = 'none';
+    loadingElement.style.display = '';
+
+})
